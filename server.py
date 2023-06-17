f2f0cbd74e10d938fa9142cd346ee060de8eea4d
igor.piatigorets <igor.piatigorets@nikel.com.ua>
2023-06-17 17:52:06 +0300

    feat(server): Now, when requesting a non-existent or empty resource, the user is shown an error page, minor refactoring

--- server.py
+++ server.py
@@ -1,51 +1,51 @@
+"""Module providing function of routing and request handler."""
+import importlib
+import os
+import sys
+from typing import Any
+
 from flask import Flask
 from flask import render_template
-from flask import request
 from flask import url_for
-import importlib
-import os, sys
 
 importlib.reload(sys)
 
-
 app = Flask(__name__)
 
-video_dir = 'static/videos'
-suffix=['avi','rmvb' ,'mpg' ,'mpeg' ,'mpe' ,'wmv' ,'mp4' ,'mkv']
+VIDEO_DIR = 'static/videos'
+SUFFIX = ['avi', 'rmvb', 'mpg', 'mpeg', 'mpe', 'wmv', 'mp4', 'mkv']
 
-@app.route('/', defaults={'syspath':''})
+
+@app.route('/', defaults={'syspath': ''})
 @app.route('/<path:syspath>')
-def dir_expand(syspath):
-	global suffix
-	print('syspath:',syspath)
-	server_path=video_dir+'/'+syspath
-	print('server_path',server_path)
-	if os.path.isdir(server_path):
-		print('path:',server_path)
-		dir_list=[d for d in os.listdir(server_path)]
-		dirs=[]
-		videos=[]
-		for file in dir_list:
-			file_path=os.path.join(syspath,file)
-			file_path=file_path.replace('\\','/')
-			abs_file_path=os.path.join(server_path,file)
-			abs_path=url_for('dir_expand',syspath=file_path,_external=True)
-			print(abs_file_path)
-			if os.path.isdir(abs_file_path):
-				dirs.append([file,file_path,abs_path])
-			else:
-				if file.split('.')[-1] not in suffix:
-					continue;
-				videos.append([file,file_path,abs_path])
-		
-		return render_template("index.html",dirs=dirs,videos=videos)
-	else:
-		temp_video='videos/'+syspath
-		
-		video_src=url_for('static',filename=temp_video)
-	
-		return render_template('player.html',video_path=video_src)
+def dir_expand(syspath: Any) -> Any:
+    """
+    Entrypoint
+    """
+    server_path: str = f"{VIDEO_DIR}/{syspath}"
+    if not os.path.exists(server_path):
+        return render_template('404.html')
+    if os.path.isfile(server_path):
+        video_src: str = url_for('static', filename=f'videos/{syspath}')
+        return render_template('player.html', video_path=video_src)
 
+    if root := os.listdir(server_path):
+        dirs = []
+        videos = []
+        for child in root:
+            file_path: str = os.path.join(syspath, child)
+            abs_file_path: str = os.path.join(server_path, child)
+            abs_path: str = url_for(
+                'dir_expand', syspath=file_path, _external=True)
+
+            if os.path.isdir(abs_file_path):
+                dirs.append([child, file_path, abs_path])
+            else:
+                if child.split('.')[-1].lower() not in SUFFIX:
+                    continue
+                videos.append([child, file_path, abs_path])
+        return render_template('index.html', dirs=dirs, videos=videos)
+    return render_template('404.html')
 
 
 if __name__ == '__main__':
